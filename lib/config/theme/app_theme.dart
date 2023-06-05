7134fc5391634899b6e6db43ce182bfc42527a1a
Max Alva Garcia <alva85@gmail.com>
2023-06-05 16:18:43 -0500

    Tema y estilos de la aplicación

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.red,
+  Colors.green,
+  Colors.yellow,
+  Colors.purple,
+  Colors.orange,
+  Colors.pink,
+  Colors.teal,
+  Colors.cyan,
+  Colors.brown,
+  Colors.grey,
+  Colors.blueGrey,
+  Colors.indigo,
+  Colors.lime,
+  Colors.amber,
+  Colors.black,
+  Colors.white,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0
+  }): assert(selectedColor >= 0 && selectedColor < colorList.length, 'selectedColor must be between 0 and ${colorList.length - 1}');
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[selectedColor],
+  );
+}
