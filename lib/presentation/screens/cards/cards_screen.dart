1f2c5a245437e0ba70434c5ffb80ed1dad04942e
Max Alva Garcia <alva85@gmail.com>
2023-06-06 07:05:51 -0500

    Tarjetas con borde

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -35,9 +35,51 @@ class _CardsView extends StatelessWidget {
         children: [
           ...cards.map(
             (card) =>
+                _CardType2(label: card['label'], elevation: card['elevation']),
+          ),
+          ...cards.map(
+            (card) =>
                 _CardType1(label: card['label'], elevation: card['elevation']),
           )
         ],
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: colors.primary),
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+      ),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - Outline'),
+            )
+          ],
+        ),
       ),
     );
   }
