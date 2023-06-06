950d6923c9c646e6615b23f12fdfe485a3802fe2
Max Alva Garcia <alva85@gmail.com>
2023-06-06 06:47:23 -0500

    Boton personalizado

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -86,8 +86,35 @@ class _ButtonsView extends StatelessWidget {
                   iconColor: MaterialStateProperty.all(Colors.white),
                 ),
               ),
+              const CustomButton()
             ],
           )),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Boton personalizado',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
