dac4bbe4d07d271e6cad496a8594fdf23fad97c6
Max Alva Garcia <alva85@gmail.com>
2023-06-06 06:41:23 -0500

    Diferentes botones pre-configurados

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -11,7 +11,83 @@ class ButtonsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Botones'),
       ),
-      body: const Placeholder(),
+      body: const _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_back_ios_rounded),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          child: Wrap(
+            spacing: 10,
+            alignment: WrapAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Elevated'),
+              ),
+              const ElevatedButton(
+                onPressed: null,
+                child: Text('Elevated disabled'),
+              ),
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.access_alarm_rounded),
+                label: const Text('Elevated icon'),
+              ),
+              FilledButton(
+                onPressed: () {},
+                child: const Text('Filled'),
+              ),
+              FilledButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.accessibility_new),
+                label: const Text('Filled icon'),
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                child: const Text('Outlined'),
+              ),
+              OutlinedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.terminal),
+                label: const Text('Outlined icon'),
+              ),
+              TextButton(onPressed: () {}, child: const Text('Text')),
+              TextButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.text_fields_rounded),
+                label: const Text('Text icon'),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(colors.primary),
+                  iconColor: MaterialStateProperty.all(Colors.white),
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
