cb0ccbdbdaea8bcd124e5f59c787c0d68af4a824
Max Alva Garcia <alva85@gmail.com>
2023-06-06 08:52:28 -0500

    Snackbars

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -5,13 +5,34 @@ class SnackbarScreen extends StatelessWidget {
 
   const SnackbarScreen({super.key});
 
+  void showCustomSnackbar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: const Text('Hola, soy un snackbar'),
+        duration: const Duration(seconds: 2),
+        action: SnackBarAction(
+          label: 'Ok!',
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('SnackbarScreen'),
+        title: const Text('Snackbars y Dialogos'),
       ),
-      body: const Placeholder(),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Mostrar snackbar'),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        onPressed: () {
+          showCustomSnackbar(context);
+        },
+      ),
     );
   }
 }
