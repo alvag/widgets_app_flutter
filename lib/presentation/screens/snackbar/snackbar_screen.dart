663d908c7b0bb3b8c64fd8e421c6e774569cae7a
Max Alva Garcia <alva85@gmail.com>
2023-06-06 09:01:54 -0500

    Dialogos y licencias

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -20,19 +20,63 @@ class SnackbarScreen extends StatelessWidget {
     );
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Hola, soy un dialogo'),
+          content: const Text('Este es el contenido del dialogo'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cerrar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Snackbars y Dialogos'),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        label: const Text('Mostrar snackbar'),
-        icon: const Icon(Icons.remove_red_eye_outlined),
-        onPressed: () {
-          showCustomSnackbar(context);
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Snackbars y Dialogos'),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          label: const Text('Mostrar snackbar'),
+          icon: const Icon(Icons.remove_red_eye_outlined),
+          onPressed: () {
+            showCustomSnackbar(context);
+          },
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FilledButton.tonal(
+                onPressed: () {
+                  showAboutDialog(
+                    context: context,
+                    children: [
+                      const Text('Este es un dialogo de licencias'),
+                    ],
+                  );
+                },
+                child: const Text('Licencias usadas'),
+              ),
+              FilledButton.tonal(
+                onPressed: () {
+                  openDialog(context);
+                },
+                child: const Text('Mostrar dialogo'),
+              )
+            ],
+          ),
+        ));
   }
 }
