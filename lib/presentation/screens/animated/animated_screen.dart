e07de33f0fdcadb7c0f64818334fe3d422fed1d3
Max Alva Garcia <alva85@gmail.com>
2023-06-07 05:55:48 -0500

    Animar propiedades del contenedor

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,9 +1,36 @@
+import 'dart:math' show Random;
+
 import 'package:flutter/material.dart';
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   static const routeName = 'animated_screen';
 
   const AnimatedScreen({super.key});
+
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double width = 50;
+  double height = 50;
+  Color color = Colors.indigo;
+  double borderRadius = 10;
+
+  void changeShape() {
+    final random = Random();
+    setState(() {
+      width = random.nextInt(300).toDouble();
+      height = random.nextInt(300).toDouble();
+      color = Color.fromRGBO(
+        random.nextInt(256),
+        random.nextInt(256),
+        random.nextInt(256),
+        1,
+      );
+      borderRadius = random.nextInt(100).toDouble();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,18 +39,19 @@ class AnimatedScreen extends StatelessWidget {
         title: const Text('Animated Container'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: changeShape,
         child: const Icon(Icons.play_arrow_rounded),
       ),
       body: Center(
         child: AnimatedContainer(
           duration: const Duration(milliseconds: 400),
-          width: 200,
-          height: 200,
+          width: width <= 0 ? 0 : width,
+          height: height <= 0 ? 0 : height,
           curve: Curves.easeOutCubic,
           decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(20),
+            color: color,
+            borderRadius:
+                BorderRadius.circular(borderRadius <= 0 ? 0 : borderRadius),
           ),
         ),
       ),
